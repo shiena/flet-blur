810f8e633798e613c85f1cd4e03e5dc7613766ff
KOGA Mitsuhiro <shiena.jp@gmail.com>
2025-03-02 02:23:40 +0900

    Implement examples

--- examples/blur_example/src/main.py
+++ examples/blur_example/src/main.py
@@ -1,20 +1,26 @@
 import flet as ft
 
-from blur import Blur
-
+import blur as ftb
 
 def main(page: ft.Page):
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    bgblur = ftb.Blur()
 
+    def on_click(v: ftb.WindowEffect):
+        async def f(e):
+            await bgblur.set_window_effect(v)
+        return f
+
+    page.overlay.append(bgblur)
+    page.bgcolor = ft.Colors.TRANSPARENT
     page.add(
-
-                ft.Container(height=150, width=300, alignment = ft.alignment.center, bgcolor=ft.Colors.PURPLE_200, content=Blur(
-                    tooltip="My new Blur Control tooltip",
-                    value = "My new Blur Flet Control", 
-                ),),
-
+        ft.Text("Acrylic!"),
+        ft.ElevatedButton("transparent", on_click=on_click(ftb.WindowEffect.TRANSPARENT)),
+        ft.ElevatedButton("solid", on_click=on_click(ftb.WindowEffect.SOLID)),
+        ft.ElevatedButton("aero", on_click=on_click(ftb.WindowEffect.AERO)),
+        ft.ElevatedButton("acrylic", on_click=on_click(ftb.WindowEffect.ACRYLIC)),
+        ft.ElevatedButton("mica", on_click=on_click(ftb.WindowEffect.MICA)),
+        ft.ElevatedButton("tabbed", on_click=on_click(ftb.WindowEffect.TABBED)),
+        ft.ElevatedButton("disabled", on_click=on_click(ftb.WindowEffect.DISABLED)),
     )
-
 
 ft.app(main)
